7693b2dbc1c45afd0f87d122a6e6e024826b6a53
Vijay5577 <92287774+Vijay5577@users.noreply.github.com>
2021-10-25 11:01:14 +0530

    Add files via upload

---
+++ Marks - Copy.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	float phy, che, mat, eng, tot;
+	
+	//initialization
+	phy = 60.5454;
+	che = 80.586;
+	mat = 75.258;
+	eng = 90.365;
+	tot = phy+che+mat+eng; 
+	
+	//output
+	 printf("marks in phy : %f\n",phy);
+	 printf("marks in che : %f\n", che);
+	 printf("marks in mat : %f\n", mat);
+	 printf("marks in eng : %f\n", eng);
+	 printf("total : %f", tot);
+
+}
