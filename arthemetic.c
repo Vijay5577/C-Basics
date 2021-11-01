3a5999f468a01c130f409c893c09a88cc0e81c9b
Vijay5577 <92287774+Vijay5577@users.noreply.github.com>
2021-11-01 10:55:57 +0530

    Add files via upload

---
+++ arthemetic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+//character declaration
+int a,b,s,d,p,q,r;
+a=10;
+b=2;
+s=a+b;
+d=a-b;
+p=a*b;
+q=a/b;
+r=a%b;
+printf("Sum of %d and %d is %d\n",a,b,s);
+printf("Difference of %d and %d is d%\n ",a,b,d);
+printf("Product of %d and %d is %d\n",a,b,p);
+printf("Quetiont of %d and %d is %d\n",a,b,q);
+printf("Remainder of %d and %d is %d\n",a,b,r);
+printf("Sum of all arthemetic operations is%d\n ",s+d+p+q+r);
+}
